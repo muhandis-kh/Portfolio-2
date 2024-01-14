22234a307a60f0a30b2a0f3934876d48e17586a9
okh-engineer <ohojimirzayev@gmail.com>
2024-01-14 23:05:46 +0500

    Add download_cv button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
+import cv from "../Hojimirzayev_Obidxon_CV.pdf"
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
@@ -52,6 +53,13 @@ const About = () => {
          I'm a fast learner and quickly grasp information, I want to create effective,
           scalable and user-friendly projects for people to simplify their lives and to solve real problems. 
       </motion.p>
+      <button class="border-#915EFF mt-10 bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+      <svg class="fill-current w-4 h-4 mr-2 text-[#915EFF]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+
+        <a href={cv} download="" class="text-[#915EFF]">
+            Download Resume
+        </a>
+      </button>
 
       <div className='mt-20 flex flex-wrap gap-10 cursor-pointer'>
         {services.map((service, index) => (
